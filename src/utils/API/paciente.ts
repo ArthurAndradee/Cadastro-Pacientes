7caac7f48f33f40e82653a517e8ca65fba8103a9
arthur andrade <arthurandrad.23@gmail.com>
2024-09-04 21:55:02 -0300

    Fix: Removed unecessary docs

--- src/utils/API/paciente.ts
+++ src/utils/API/paciente.ts
@@ -1,2 +1,12 @@
 import axios from "axios";
 import { Paciente } from "../interfaces/paciente/paciente";
+
+export const getPatients = async (): Promise<Paciente[]> => {
+    try {
+      const response = await axios.get('urgetrauma.pulseti.com:9000/');
+      return response.data;
+    } catch (error) {
+      console.error('Erro requisitando pacientes: ', error);
+      return [];
+    }
+  };
